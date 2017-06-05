015d671c58dd5cd18ba17daa5570a81f7986f9bd
gdeskos-ic <gd1414@ic.ac.uk>
2017-06-05 09:44:24 +0100

    added the module_param.f90

--- module_param.f90
+++ module_param.f90
@@ -48,7 +48,7 @@ module variables
 !2-->every 2 mesh nodes
 !4-->every 4 mesh nodes
 !nvisu = size for visualization collection
-integer,parameter :: nx=241,ny=49,nz=49
+integer,parameter :: nx=481,ny=97,nz=97
 integer,parameter :: nstat=2,nvisu=1
 integer,parameter :: p_row=2,p_col=2
 integer,parameter :: nxm=nx-1,nym=ny-1,nzm=nz-1
