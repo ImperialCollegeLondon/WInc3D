45b145383e864215e1ab51a4c85d84d01dcf7755
GD_mac <gdeskosv@gmail.com>
2018-05-18 12:31:15 +0100

    removed the filtering

--- ABL.f90
+++ ABL.f90
@@ -49,9 +49,9 @@ if (nrank==0) print *, 'Max of the filtered velocity', maxval(uxf), 'Max of the 
     if (xstart(2)==1) then 
     do k=1,xsize(3)
     do i=1,xsize(1)
-        ux_HAve_local=ux_HAve_local+0.5*(uxf(i,1,k)+uxf(i,2,k))
-        uz_HAve_local=uz_HAve_local+0.5*(uzf(i,1,k)+uzf(i,2,k))
-        S_HAve_local=S_HAve_local+sqrt((0.5*(uxf(i,1,k)+uxf(i,2,k)))**2.+(0.5*(uzf(i,1,k)+uzf(i,2,k)))**2.)
+        ux_HAve_local=ux_HAve_local+0.5*(ux(i,1,k)+ux(i,2,k))
+        uz_HAve_local=uz_HAve_local+0.5*(uz(i,1,k)+uz(i,2,k))
+        S_HAve_local=S_HAve_local+sqrt((0.5*(ux(i,1,k)+ux(i,2,k)))**2.+(0.5*(uz(i,1,k)+uz(i,2,k)))**2.)
         sxy_HAve_local=sxy_HAve_local+sxy1(i,2,k)
         szy_HAve_local=szy_HAve_local+syz1(i,2,k)
         nut_HAve_local=nut_HAve_local+nut1(i,2,k)
@@ -140,8 +140,8 @@ if (nrank==0) print *, 'Max of the filtered velocity', maxval(uxf), 'Max of the 
     if (xstart(2)==1) then
     do k=1,xsize(3)
     do i=1,xsize(1)                       
-    ux12=0.5*(uxf(i,1,k)+uxf(i,2,k)) 
-    uz12=0.5*(uzf(i,1,k)+uzf(i,2,k))
+    ux12=0.5*(ux(i,1,k)+ux(i,2,k)) 
+    uz12=0.5*(uz(i,1,k)+uz(i,2,k))
     S12=sqrt(ux12**2.+uz12**2.)
 
     if(iwallmodel==1) then ! MOENG    
