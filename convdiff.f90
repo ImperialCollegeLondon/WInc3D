f5f66ef9da5554b3faa4e5d77643167d25947c70
gdeskos <gdeskosv@gmail.com>
2018-11-02 16:55:09 +0000

    fewer bugs hopefully from now on

--- convdiff.f90
+++ convdiff.f90
@@ -512,7 +512,7 @@ end subroutine convdiff
 
 !************************************************************
 !
-subroutine PotentialTemperature(ux1,uy1,uz1,phi1,phis1,phiss1,di1,ta1,tb1,tc1,td1,&
+subroutine PotentialTemperature(ux1,uy1,uz1,nut1,phi1,phis1,phiss1,di1,ta1,tb1,tc1,td1,&
      uy2,uz2,phi2,di2,ta2,tb2,tc2,td2,uz3,phi3,di3,ta3,tb3,epsi)
 !
 !************************************************************
@@ -522,7 +522,7 @@ USE decomp_2d
 
 implicit none
 
-real(mytype),dimension(xsize(1),xsize(2),xsize(3)) :: ux1,uy1,uz1,phi1,phis1,&
+real(mytype),dimension(xsize(1),xsize(2),xsize(3)) :: ux1,uy1,uz1,nut1,phi1,phis1,&
                                               phiss1,di1,ta1,tb1,tc1,td1,epsi
 real(mytype),dimension(ysize(1),ysize(2),ysize(3)) :: uy2,uz2,phi2,di2,ta2,tb2,tc2,td2
 real(mytype),dimension(zsize(1),zsize(2),zsize(3)) :: uz3,phi3,di3,ta3,tb3
@@ -535,9 +535,7 @@ nvect2=ysize(1)*ysize(2)*ysize(3)
 nvect3=zsize(1)*zsize(2)*zsize(3)
 
 !X PENCILS
-do ijk=1,nvect1
-   ta1(ijk,1,1)=ux1(ijk,1,1)*phi1(ijk,1,1)
-enddo
+ta1(:,:,:)=ux1(:,:,:)*phi1(:,:,:)
 
 call derx (tb1,ta1,di1,sx,ffx,fsx,fwx,xsize(1),xsize(2),xsize(3),0)
 
@@ -552,9 +550,7 @@ call transpose_x_to_y(uy1,uy2)
 call transpose_x_to_y(uz1,uz2)
 
 !Y PENCILS
-do ijk=1,nvect2
-   ta2(ijk,1,1)=uy2(ijk,1,1)*phi2(ijk,1,1)
-enddo
+ta2(:,:,:)=uy2(:,:,:)*phi2(:,:,:)
 
 call dery(tb2,ta2,di2,sy,ffy,fsy,fwy,ppy,ysize(1),ysize(2),ysize(3),0)
 
@@ -585,11 +581,9 @@ call transpose_y_to_z(phi2,phi3)
 call transpose_y_to_z(uz2,uz3)
 
 !Z PENCILS
-do ijk=1,nvect3
-   ta3(ijk,1,1)=uz3(ijk,1,1)*phi3(ijk,1,1)
-enddo
+ta3(:,:,:)=uz3(:,:,:)*phi3(:,:,:)
 
-call derz (tb3,ta3,di3,sz,ffz,fsz,fwz,zsize(1),zsize(2),zsize(3),0)
+call derz(tb3,ta3,di3,sz,ffz,fsz,fwz,zsize(1),zsize(2),zsize(3),0)
 
 if (jles==1) then
     call derzz_iles (ta3,phi3,di3,sz,sfzp,sszp,swzp,zsize(1),zsize(2),zsize(3),1)
@@ -600,50 +594,35 @@ call transpose_z_to_y(ta3,tc2)
 call transpose_z_to_y(tb3,td2)
 
 !Y PENCILS ADD TERMS
-do ijk=1,nvect2
-   tc2(ijk,1,1)=tc2(ijk,1,1)+ta2(ijk,1,1)
-   td2(ijk,1,1)=td2(ijk,1,1)+tb2(ijk,1,1)
-enddo
+tc2(:,:,:)=tc2(:,:,:)+ta2(:,:,:)
+td2(:,:,:)=td2(:,:,:)+tb2(:,:,:)
 
 call transpose_y_to_x(tc2,tc1)
 call transpose_y_to_x(td2,td1)
 
 !X PENCILS ADD TERMS
-do ijk=1,nvect1
-   ta1(ijk,1,1)=ta1(ijk,1,1)+tc1(ijk,1,1) !SECOND DERIVATIVE
-   tb1(ijk,1,1)=tb1(ijk,1,1)+td1(ijk,1,1) !FIRST DERIVATIVE
-enddo
+ta1(:,:,:)=ta1(:,:,:)+tc1(:,:,:) !SECOND DERIVATIVE
+tb1(:,:,:)=tb1(:,:,:)+td1(:,:,:) !FIRST DERIVATIVE
  
-do ijk=1,nvect1
-   if(jles==1) then
-   ta1(ijk,1,1)=xnu/Pr*ta1(ijk,1,1)-tb1(ijk,1,1) 
-   else
-    if (nrank==0) print *,'Not ready'
-    stop 
-   endif
-enddo
+if(jles==1) then
+ta1(:,:,:)=xnu/Pr*ta1(:,:,:)-tb1(:,:,:) 
+else
+ta1(:,:,:)=(xnu+nut1(:,:,:))/Pr*ta1(:,:,:)-tb1(:,:,:) 
+endif
 
-!TIME ADVANCEMENT
-nxyz=xsize(1)*xsize(2)*xsize(3)  
-
+!TIME ADVANCEMENT (Doing it locally)
 if ((nscheme.eq.1).or.(nscheme.eq.2)) then
    if ((nscheme.eq.1.and.itime.eq.1.and.ilit.eq.0).or.&
         (nscheme.eq.2.and.itr.eq.1)) then
-      do ijk=1,nxyz
-         phi1(ijk,1,1)=gdt(itr)*ta1(ijk,1,1)+phi1(ijk,1,1)
-         phis1(ijk,1,1)=ta1(ijk,1,1)          
-      enddo
+        phi1(:,:,:)=gdt(itr)*ta1(:,:,:)+phi1(:,:,:)
+        phis1(:,:,:)=ta1(:,:,:)          
    else
-      do ijk=1,nxyz
-         phi1(ijk,1,1)=adt(itr)*ta1(ijk,1,1)+bdt(itr)*phis1(ijk,1,1)+phi1(ijk,1,1)
-         phis1(ijk,1,1)=ta1(ijk,1,1)          
-      enddo
+         phi1(:,:,:)=adt(itr)*ta1(:,:,:)+bdt(itr)*phis1(:,:,:)+phi1(:,:,:)
+         phis1(:,:,:)=ta1(:,:,:)          
 endif
 endif
 
 if (nscheme.eq.3) then 
-if (nrank==0) print *,'Not ready'
-stop 
 endif
 
 if (nscheme==4) then
@@ -673,143 +652,4 @@ if (nscheme==4) then
 endif
 
 end subroutine PotentialTemperature
-
-
-!************************************************************
-!
-subroutine scalar(ux1,uy1,uz1,phi1,phis1,phiss1,di1,ta1,tb1,tc1,td1,&
-     uy2,uz2,phi2,di2,ta2,tb2,tc2,td2,uz3,phi3,di3,ta3,tb3,epsi)
-!
-!************************************************************
-
-USE param
-USE decomp_2d
-
-implicit none
-
-real(mytype),dimension(xsize(1),xsize(2),xsize(3)) :: ux1,uy1,uz1,phi1,phis1,&
-                                              phiss1,di1,ta1,tb1,tc1,td1,epsi
-real(mytype),dimension(ysize(1),ysize(2),ysize(3)) :: uy2,uz2,phi2,di2,ta2,tb2,tc2,td2
-real(mytype),dimension(zsize(1),zsize(2),zsize(3)) :: uz3,phi3,di3,ta3,tb3
-
-integer :: ijk,nvect1,nvect2,nvect3,i,j,k,nxyz
-real(mytype) :: x,y,z
-
-nvect1=xsize(1)*xsize(2)*xsize(3)
-nvect2=ysize(1)*ysize(2)*ysize(3)
-nvect3=zsize(1)*zsize(2)*zsize(3)
-
-!X PENCILS
-do ijk=1,nvect1
-   ta1(ijk,1,1)=ux1(ijk,1,1)*phi1(ijk,1,1)
-enddo
-call derx (tb1,ta1,di1,sx,ffx,fsx,fwx,xsize(1),xsize(2),xsize(3),0)
-call derxx (ta1,phi1,di1,sx,sfxp,ssxp,swxp,xsize(1),xsize(2),xsize(3),1)
-
-call transpose_x_to_y(phi1,phi2)
-call transpose_x_to_y(uy1,uy2)
-call transpose_x_to_y(uz1,uz2)
-
-!Y PENCILS
-do ijk=1,nvect2
-   ta2(ijk,1,1)=uy2(ijk,1,1)*phi2(ijk,1,1)
-enddo
-call dery (tb2,ta2,di2,sy,ffy,fsy,fwy,ppy,ysize(1),ysize(2),ysize(3),0)
-if (istret.ne.0) then 
-   call deryy (ta2,phi2,di2,sy,sfyp,ssyp,swyp,ysize(1),ysize(2),ysize(3),1)
-   call dery (tc2,phi2,di2,sy,ffy,fsy,fwy,ppy,ysize(1),ysize(2),ysize(3),0)
-   do k=1,ysize(3)
-   do j=1,ysize(2)
-   do i=1,ysize(1)
-      ta2(i,j,k)=ta2(i,j,k)*pp2y(j)-pp4y(j)*tc2(i,j,k)
-   enddo
-   enddo
-   enddo
-else
-   call deryy (ta2,phi2,di2,sy,sfyp,ssyp,swyp,ysize(1),ysize(2),ysize(3),1) 
-endif
-
-call transpose_y_to_z(phi2,phi3)
-call transpose_y_to_z(uz2,uz3)
-
-!Z PENCILS
-do ijk=1,nvect3
-   ta3(ijk,1,1)=uz3(ijk,1,1)*phi3(ijk,1,1)
-enddo
-call derz (tb3,ta3,di3,sz,ffz,fsz,fwz,zsize(1),zsize(2),zsize(3),0)
-call derzz (ta3,phi3,di3,sz,sfzp,sszp,swzp,zsize(1),zsize(2),zsize(3),1)
-
-call transpose_z_to_y(ta3,tc2)
-call transpose_z_to_y(tb3,td2)
-
-!Y PENCILS ADD TERMS
-do ijk=1,nvect2
-   tc2(ijk,1,1)=tc2(ijk,1,1)+ta2(ijk,1,1)
-   td2(ijk,1,1)=td2(ijk,1,1)+tb2(ijk,1,1)
-enddo
-
-call transpose_y_to_x(tc2,tc1)
-call transpose_y_to_x(td2,td1)
-
-!X PENCILS ADD TERMS
-do ijk=1,nvect1
-   ta1(ijk,1,1)=ta1(ijk,1,1)+tc1(ijk,1,1) !SECOND DERIVATIVE
-   tb1(ijk,1,1)=tb1(ijk,1,1)+td1(ijk,1,1) !FIRST DERIVATIVE
-enddo
- 
-do ijk=1,nvect1
-   ta1(ijk,1,1)=xnu/sc*ta1(ijk,1,1)-tb1(ijk,1,1) 
-enddo
-
-!TIME ADVANCEMENT
-nxyz=xsize(1)*xsize(2)*xsize(3)  
-
-if ((nscheme.eq.1).or.(nscheme.eq.2)) then
-   if ((nscheme.eq.1.and.itime.eq.1.and.ilit.eq.0).or.&
-        (nscheme.eq.2.and.itr.eq.1)) then
-      do ijk=1,nxyz
-         phi1(ijk,1,1)=gdt(itr)*ta1(ijk,1,1)+phi1(ijk,1,1)
-         phis1(ijk,1,1)=ta1(ijk,1,1)          
-      enddo
-   else
-      do ijk=1,nxyz
-         phi1(ijk,1,1)=adt(itr)*ta1(ijk,1,1)+bdt(itr)*phis1(ijk,1,1)+phi1(ijk,1,1)
-         phis1(ijk,1,1)=ta1(ijk,1,1)          
-      enddo
-endif
-endif
-
-if (nscheme.eq.3) then 
-if (nrank==0) print *,'Not ready'
-stop 
-endif
-
-if (nscheme==4) then
-   if ((itime.eq.1).and.(ilit.eq.0)) then
-      if (nrank==0) print *,'start with Euler',itime
-      do ijk=1,nxyz !start with Euler
-         phi1(ijk,1,1)=dt*ta1(ijk,1,1)+phi1(ijk,1,1)
-         phis1(ijk,1,1)=ta1(ijk,1,1)          
-      enddo
-   else
-      if  ((itime.eq.2).and.(ilit.eq.0)) then
-         if (nrank==0) print *,'then with AB2',itime
-         do ijk=1,nxyz
-            phi1(ijk,1,1)=1.5*dt*ta1(ijk,1,1)-0.5*dt*phis1(ijk,1,1)+phi1(ijk,1,1)
-            phiss1(ijk,1,1)=phis1(ijk,1,1)
-            phis1(ijk,1,1)=ta1(ijk,1,1)
-         enddo 
-      else
-         do ijk=1,nxyz
-            phi1(ijk,1,1)=adt(itr)*ta1(ijk,1,1)+bdt(itr)*phis1(ijk,1,1)+&
-                 cdt(itr)*phiss1(ijk,1,1)+phi1(ijk,1,1)
-            phiss1(ijk,1,1)=phis1(ijk,1,1)
-            phis1(ijk,1,1)=ta1(ijk,1,1)
-         enddo
-      endif
-   endif
-endif
-
-
- end subroutine scalar
 
